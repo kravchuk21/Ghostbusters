9ae203f2a0fcaa5e6acc0b6ca7ca8d3fdb76011c
VladKrauchuk <66268773+VladKrauchuk@users.noreply.github.com>
2020-06-19 16:41:41 +0300

    Day 4 update

---
+++ js/main.js
@@ -0,0 +1,36 @@
+$(document).ready(function () {
+  const mButtons = $(".mobile-buttons");
+  const mMenu = $(".m-menu");
+  const tab = $(".tab");
+  mButtons.on("click", function () {
+    mMenu.toggleClass("active");
+    $("body").toggleClass("no-scroll");
+    $(".swiper-container").toggleClass("active");
+  });
+  tab.on("click", function () {
+    tab.removeClass("active");
+    $(this).toggleClass("active");
+    let activeTabContent = $(this).attr("data-target");
+    $(".tabs-content").removeClass("visible");
+    $(activeTabContent).toggleClass("visible");
+  });
+  let mySwiper = new Swiper(".swiper-container", {
+    slidesPerView: 4,
+    spaceBetween: 25,
+    loop: true,
+    breakpoints: {
+      991: {
+        slidesPerView: 4,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      320: {
+        slidesPerView: 1,
+        navigation: {
+          nextEl: ".button-next",
+        },
+      },
+    },
+  });
+});
